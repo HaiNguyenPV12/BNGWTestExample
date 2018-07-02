e31b47fc3a2d7207d1c598a15ee6dd6f37b9fc2c
HaiNguyenPV12 <39506270+HaiNguyenPV12@users.noreply.github.com>
2018-07-02 22:19:48 +0700

    Functions for Script1 and cleaned up HTML
    Made L/R num variable a global and finished adding buttons' functions.

--- Script1.js
+++ Script1.js
@@ -1,18 +1,16 @@
 // JavaScript source code
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min; //This JavaScript function always returns a random number between min and max (both included)
 }
 
-for (var i = 0; i < document.gameform.btn[i].disabled; i++) {
-    document.gameform.btn[i].disabled = true;
-}
+var leftnum;
+var rightnum;
+var correctans = 0;
+var incorrectans = 0;
 
 function StartGame() {
-    for (var i = 0; i < document.gameform.btn; i++) {
-        document.btn[i].disabled = false;
-    }
-    var leftnum = getRndInteger(1, 10);
-    var rightnum = getRndInteger(1, 10);
+    leftnum = getRndInteger(1, 10);
+    rightnum = getRndInteger(1, 10);
     var x = "";
     var y = "";
     for (var i = 0; i < leftnum; i++) {
@@ -25,6 +23,59 @@ function StartGame() {
     document.gameform.txtright.value = y;
 
     for (var i = 0; i < document.gameform.btn.length; i++) {
-        document.gameform.btn[i].disabled = false;
+        document.gameform.btn[i].removeAttribute("disabled");
     }
+}
+function disablebuttons() {
+    for (var i = 0; i < document.gameform.btn.length; i++) {
+        document.gameform.btn[i].setAttribute("disabled", "disabled");
+    }
+}
+
+function compare(id) {
+    switch (id) {
+        case "a>b":
+            if (leftnum > rightnum) {
+                correctans = correctans + 1;
+                results();
+            }
+            else {
+                incorrectans = incorrectans + 1;
+                results();
+            }
+            disablebuttons();
+            break;
+        case "a=b":
+            if (leftnum == rightnum) {
+                correctans = correctans + 1;
+                results();
+            }
+            else {
+                incorrectans = incorrectans + 1;
+                results();
+            }
+            disablebuttons();
+            break;
+        case "a<b":
+            if (leftnum < rightnum) {
+                correctans = correctans + 1;
+                results();
+            }
+            else {
+                incorrectans = incorrectans + 1;
+                results();
+            }
+            disablebuttons();
+            break;
+
+        default:
+            break;
+    }
+}
+
+function results() {
+    var percentans = (correctans * 100) / (incorrectans + correctans);
+    document.gameform.canswers.value = correctans;
+    document.gameform.icanswers.value = incorrectans;
+    document.gameform.panswers.value = percentans.toFixed(1);
 }
